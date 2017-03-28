d59ef13250ed78f8a567b46ae297462ec5a8a2d4
Noemie <noemie.clay@hotmail.fr>
2017-03-28 21:47:27 +0200

    Remove publish

--- client/main.js
+++ client/main.js
@@ -1,3 +1,16 @@
 import { Template } from 'meteor/templating';
-
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
 import './main.html';
+
+
+
+Template.body.onCreated(function bodyOnCreated() {
+  Meteor.subscribe('mesInvitations');
+});
+
+Template.body.helpers({
+	users() {
+		return Meteor.users.find();
+	},
+});
--- server/main.js
+++ server/main.js
@@ -1,5 +1,15 @@
 import { Meteor } from 'meteor/meteor';
+//Invitations = new Mongo.Collection('invitations');
 
 Meteor.startup(() => {
   // code to run on server at startup
+});
+
+Meteor.publish('mesInvitations',function(){
+  if(!this.userId){
+    return this.ready();
+  }else{
+      return Meteor.users.find({_id:this.userId},{fields:{username:1,amiAvec:1,_id:1}});
+      //return Meteor.users.find().count();
+    }
 });
